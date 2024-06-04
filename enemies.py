78f10a69ee220b8a9cb651bc8a96bcce0c29b42d
Vronst <vronst@gmail.com>
2024-06-04 21:47:58 +0200

    enemies shoot and get tired'

--- enemies.py
+++ enemies.py
@@ -10,13 +10,13 @@ class Enemy(Character):
         self.move_x = move_x
         self.move_y = move_y
         self._moves = ((self.move_x, 0), (-self.move_x, 0), (0, self.move_y), (0, -self.move_y))
-        self.melee = True
+        self._melee = True
         self.attacks = pygame.sprite.Group()
-        self.cooldown = 1000
-        self.last_cooldown = pygame.time.get_ticks()
+        self.cooldown = 2000  # cooldown time - now 2 seconds
+        self._last_cooldown = pygame.time.get_ticks()
 
     def set_moves(self, x, y):
-        self._moves = ((x, 0), (-x, 0), (0, y), (0, -y))
+        self._moves = ((x, 0), (-x, 0), (0, y), (0, -y))  # how much object will move per update
 
     def _move(self, group=None):
         # making it move dependent of player object
@@ -39,11 +39,11 @@ class Enemy(Character):
 
         moves = []
 
-        if distance_x < 0 or (distance_x >= 0 and self.rect.x > self.borders[0] - 100 and not self.melee):
+        if distance_x < 0 or (distance_x >= 0 and self.rect.x > self.borders[0] - 100 and not self._melee):
             # self.rect.move_ip(self._moves[0])
             moves.append(self._moves[0])
         if (distance_x > 0 or
-                (distance_x >= 0 and self.rect.x < self.borders[0] - self.borders[0] + 100 and not self.melee)):
+                (distance_x >= 0 and self.rect.x < self.borders[0] - self.borders[0] + 100 and not self._melee)):
             # self.rect.move_ip(self._moves[1])
             moves.append(self._moves[1])
         if distance_y < 0:
@@ -54,8 +54,8 @@ class Enemy(Character):
             moves.append(self._moves[3])
 
         for move in moves:
-
-            self.rect.move_ip(move)
+            if not self.tired():
+                self.rect.move_ip(move)
             for member in group:
                 if self.rect.colliderect(member):
                     self.rect.move_ip(-move[0], -move[1])
@@ -68,15 +68,18 @@ class MeleeEnemy(Enemy):
 
     def __init__(self, cx, cy, image, borders, player, move_x=4, move_y=4):
         super().__init__(cx, cy, image, borders, player, move_x, move_y)
-        self.cooldown = 10000
+        self.cooldown = 4000
+        self._rest = 0
 
     def tired(self) -> bool:
-        now = pygame.time.get_ticks()
-        if now - self.last_cooldown > self.cooldown:
-            self.last_cooldown = now
-            return True
-        else:
+        now = pygame.time.get_ticks()  # getting relative variable
+        if now - self._last_cooldown > self.cooldown:  # checking the difference
+            if now - self._rest > self.cooldown:  # this one ensures that Object will rest
+                self._last_cooldown = now  # this and self.rest - reset cycle
             return False
+        else:
+            self._rest = now
+            return True
 
 
 class RangeEnemy(Enemy):
@@ -84,12 +87,11 @@ class RangeEnemy(Enemy):
         super().__init__(cx, cy, image, borders, player, move_x, move_y)
         self.melee = False
 
-
     def attack(self, direction: str = 'down'):
         now = pygame.time.get_ticks()
-        if now - self.last_cooldown > self.cooldown:
+        if now - self._last_cooldown > self.cooldown:
             bullet = EnemyBullet(self.rect.x, self.rect.y, self.image, self.borders, direction, 3)
-            self.last_cooldown = now
+            self._last_cooldown = now
             return bullet
 
 
